8485078bffe775cec12656fe982f265d533b5877
17rndl <darkslayer1710@gmail.com>
2024-11-24 15:07:09 +0800

    improved the UI of the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                "theme-heading": ["'Exo 2'", "sans-serif"],
+            "theme-content": ["Alegreya Sans", "sans-serif"],
+            },
+            colors: {
+                "theme-primary": "#E8BCB9",
+                "theme-secondary": "#FA5757",
+                "theme-grayish-blue": "#3C3D37",
+                "theme-dark-blue": "rgb(37, 43, 70)",
+                "theme-dark-blue-tp": "rgba(37, 43, 70, 0.9)",
             },
         },
     },
