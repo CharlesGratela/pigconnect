d3f1028e4333b367e51b38ffcb5284978236bc73
17rndl <darkslayer1710@gmail.com>
2024-11-24 12:31:25 +0800

    revamped the landing page, added associating images (public/images), and added favicon

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+
+import WelcomeHeader from './section/WelcomeHeader.vue';
+import WelcomeHero from './section/WelcomeHero.vue';
+import WelcomeFooter from './section/WelcomeFooter.vue';
 
 defineProps({
     canLogin: {
@@ -27,56 +30,16 @@ function handleImageError() {
 </script>
 
 <template>
-  <div class="relative pt-16 pb-32 flex content-center items-center justify-center" style="min-height: 75vh;">
-    <div class="absolute top-0 w-full h-full bg-center bg-cover" style="background-image: url('pig.png');">
-      <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>
-    </div>
+  <WelcomeHeader/>
 
-    <!-- Header Section with Title and Links -->
-    <header class="absolute top-0 left-0 w-full flex justify-between items-center p-4">
-      <!-- Title on the Far Left -->
-      <div class="text-white font-bold text-xl">
-        PigConnect
-      </div>
-      
-      <!-- Links on the Far Right -->
-      <div class="flex space-x-4">
-        <Link href="/login" class="text-white font-bold py-2 px-4 rounded bg-transparent hover:underline">Login</Link>
-        <Link  href="/register" class="text-white font-bold py-2 px-4 rounded bg-transparent hover:underline">Register</Link>
-      </div>
-    </header>
-    
-    <div class="container relative mx-auto">
-      <div class="items-center flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-          <div class="pr-12">
-            <h1 class="text-white font-semibold text-5xl">PigConnect: Intelligent Pig Farming Management System</h1>
-            <p class="mt-4 text-lg text-gray-300">
-                Pigconnect is a comprehensive web app designed for pig farmers, enabling seamless tracking of herd health, growth, and productivity. Optimize your operations and stay connected to your flock with ease!
-            </p>
-          </div>
-        </div>
-      </div>
-      
-      <!-- Image Element with Error Handling -->
-      <img 
-        src="your-image-url.png" 
-        alt="Descriptive alt text" 
-        id="screenshot-container"
-        @error="handleImageError"
-        class="mx-auto mt-4" 
-      />
-      
-    </div>
-    
-    <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden" style="height: 70px; transform: translateZ(0px);">
-      <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-        <polygon class="text-gray-300 fill-current" points="2560 0 2560 100 0 100"></polygon>
-      </svg>
-    </div>
-  </div>
+  <WelcomeHero/>
+
+  <WelcomeFooter />
 </template>
+
+<style scoped></style>
 
 <script>
 export default {};
 </script>
+
